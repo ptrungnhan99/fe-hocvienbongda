be3bd4beb016afc1949310836777d932dc7ec944
DESKTOP-5NITKTU\Member <nhan.pham@firstpage.vn>
2023-06-21 17:24:43 +0700

    Update homepage v2

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,7 +4,9 @@ $(document).ready(function () {
     );
     var topMenu = $("#top-menu");
     toggleMenu.click(function () {
+        $(this).find("i").toggleClass("fa-x fa-bars");
         topMenu.toggleClass("active");
+
     });
     // -----Popup Gallery Image-----//
     $('.image-popup').magnificPopup({
@@ -16,5 +18,34 @@ $(document).ready(function () {
     // -----Popup Video-----//
     $('.video-popup').magnificPopup({
         type: 'iframe'
+    });
+    // -----Slider Home-----//
+    $('.slider-banner-home').owlCarousel({
+        loop: true,
+        margin: 10,
+        nav: true,
+        navText: [
+            '<i class="fa-regular fa-chevron-left"></i>',
+            '<i class="fa-regular fa-chevron-right"></i>',
+        ],
+        items: 1,
+        dots: false,
+
+    });
+    // -----Fix Menu-----//
+    $(window).scroll(function () {
+        var scroll = $(this);
+        $("#site-header").toggleClass(
+            "sticky slideInDown animated",
+            scroll[0].pageYOffset > 0
+        );
+    });
+    /*-- Gallery --*/
+    var galleryPopup = $('.gallery-popup');
+    galleryPopup.magnificPopup({
+        type: 'image',
+        gallery: {
+            enabled: true
+        }
     });
 });
