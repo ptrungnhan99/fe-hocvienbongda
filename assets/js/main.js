1154d1ce8c8dd583ca3449d7fc8db4e9faadf667
DESKTOP-5NITKTU\Member <nhan.pham@firstpage.vn>
2023-07-05 16:58:36 +0700

    Update Course Detail

--- assets/js/main.js
+++ assets/js/main.js
@@ -62,4 +62,27 @@ $(document).ready(function () {
         autoplayTimeout: 3000,
         autoplayHoverPause: true
     });
+    $('.slider-coach, .slider-student').owlCarousel({
+        loop: true,
+        margin: 10,
+        nav: true,
+        navText: [
+            '<i class="fa-regular fa-chevron-left"></i>',
+            '<i class="fa-regular fa-chevron-right"></i>',
+        ],
+        autoplay: true,
+        autoplayTimeout: 3000,
+        autoplayHoverPause: true,
+        responsive: {
+            0: {
+                items: 1
+            },
+            600: {
+                items: 3
+            },
+            1000: {
+                items: 4
+            }
+        }
+    })
 });
